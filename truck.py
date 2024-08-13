71119941340e69d08318d3d96e25203345b7f8af
gegjh23 <doiqjnea4@outlook.com>
2024-08-13 16:29:37 +0800

    Create truck.py

---
+++ truck.py
@@ -0,0 +1,32 @@
+# 使用大括号 {} 来创建空字典
+emptyDict = {}
+ 
+# 打印字典
+print(emptyDict)
+ 
+# 查看字典的数量
+print("Length:", len(emptyDict))
+ 
+# 查看类型
+print(type(emptyDict))
+emptyDict = dict()
+ 
+# 打印字典
+print(emptyDict)
+ 
+# 查看字典的数量
+print("Length:",len(emptyDict))
+ 
+# 查看类型
+print(type(emptyDict))
+#!/usr/bin/python3
+ 
+tinydict = {'Name': 'Runoob', 'Age': 7, 'Class': 'First'}
+ 
+print ("tinydict['Name']: ", tinydict['Name'])
+print ("tinydict['Age']: ", tinydict['Age'])
+#!/usr/bin/python3
+ 
+tinydict = {'Name': 'Runoob', 'Age': 7, 'Class': 'First'}
+ 
+print ("tinydict['Alice']: ", tinydict['Alice'])
